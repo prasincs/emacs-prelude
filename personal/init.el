b63b65d49ad5d13a58a30112278689ca5128785d
Prasanna Gautam <prasannagautam@gmail.com>
2015-09-15 03:26:29 -0700

    Finally fixed org-mode windmove conflict! Yay!

--- personal/init.el
+++ personal/init.el
@@ -95,5 +95,13 @@
 (add-hook 'js2-mode-hook 'js-mode-bindings)
 (add-hook 'json-mode 'js-mode-bindings)
 
+;; Fix conflicts (http://orgmode.org/org.html#Conflicts)
+
+;; windmove compatibility
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 (server-start)
 ;;; init.el ends here
