36ad8fff8dbcc4363a1ffe218018de0e7f232df8
Prasanna Gautam <prasannagautam@gmail.com>
2015-10-21 22:49:49 -0700

    Adding helm-dash support for Go

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,1 +1,9 @@
 (setq prelude-theme 'monokai)
+(setq helm-dash-browser-func 'eww)
+
+(defun go-doc ()
+  (interactive)
+  (setq-local helm-dash-docsets '("Go")))
+
+(add-hook 'go-mode-hook 'go-doc)
+(server-start)
