3eb0cc82de5cf8b0893714bdd971f1d356061534
Prasanna Gautam <prasannagautam@gmail.com>
2015-11-06 18:40:27 -0800

    Syncing local changes -- added snippets and such

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -3,4 +3,9 @@
 
 
 (setq helm-dash-common-docsets '("Go" "Clojure" "Emacs_Lisp") )
+
+(when (require 'yasnippet nil 'noerror)
+  (progn
+    (yas/load-directory "~/.emacs.d/snippets/clojure-mode")))
+
 (server-start)
